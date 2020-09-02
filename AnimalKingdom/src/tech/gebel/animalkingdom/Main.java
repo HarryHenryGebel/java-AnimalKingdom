35c4a69599428154c5a3c4121482fb7ae8cdedee
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-02 16:17:20 -0400

    start main function

---
+++ AnimalKingdom/src/tech/gebel/animalkingdom/Main.java
@@ -0,0 +1,14 @@
+package tech.gebel.animalkingdom;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Mammal panda = new Mammal(1869, "Panda");
+    Mammal zebra = new Mammal(1778, "Zebra");
+    Mammal koala = new Mammal(1816, "Koala");
+    Mammal sloth = new Mammal(1804, "Sloth");
+    Mammal armadillo = new Mammal(1758, "Armadillo");
+    Mammal racoon = new Mammal(1758, "Raccoon");
+    Mammal bigfoot = new Mammal(2021, "Bigfoot");
+  }
+}
